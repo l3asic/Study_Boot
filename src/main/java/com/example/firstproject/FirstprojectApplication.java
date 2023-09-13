fa6205eff1899ad98344bbe30b6cc36cc9fb1cf3
l3jh <jhcx7129@naver.com>
2023-09-14 01:57:39 +0900

    프로젝트 첫번째 revert 테스트

--- src/main/java/com/example/firstproject/FirstprojectApplication.java
+++ src/main/java/com/example/firstproject/FirstprojectApplication.java
@@ -9,6 +9,7 @@ public class FirstprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstprojectApplication.class, args);
 		// 커밋 테스트 주석 코드 22
+		// 2차 커밋 테스트
 
 	}
 
