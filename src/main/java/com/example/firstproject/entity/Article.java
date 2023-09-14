126bddb1be6227467cae9251e5f66592e0363865
l3jh <jhcx7129@naver.com>
2023-09-14 16:54:15 +0900

    11강까지  폼 동작, 엔티티화, 메모리 디비 인서트

---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,44 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+
+    @Id     // pk
+    @GeneratedValue     // 자동생성 어노테이션
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    // 이거 추가 안하면 왜 빨간줄?
+    public Article() {
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+
+
+}
